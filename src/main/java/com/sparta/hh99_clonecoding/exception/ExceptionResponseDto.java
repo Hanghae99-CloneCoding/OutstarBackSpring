0513b2fc1a6666be8e3d1f0222afc0799ad999c2
Hyunggi <phg9587@naver.com>
2022-04-15 19:57:28 +0900

    -ExceptionResponseDto 내용 수정 => Data 담을 수 있게 변경

--- src/main/java/com/sparta/hh99_clonecoding/exception/ExceptionResponseDto.java
+++ src/main/java/com/sparta/hh99_clonecoding/exception/ExceptionResponseDto.java
@@ -10,9 +10,16 @@ import lombok.Setter;
 public class ExceptionResponseDto {
     private String code;
     private String msg;
+    private Object data;
 
     public ExceptionResponseDto(Code code) {
         this.code = code.getCode();
         this.msg = code.getMsg();
+    }
+
+    public ExceptionResponseDto(Code code, Object data){
+        this.code = code.getCode();
+        this.msg = code.getMsg();
+        this.data = data;
     }
 }
