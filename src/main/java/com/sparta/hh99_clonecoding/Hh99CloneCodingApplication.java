119c9349cc18f3640745ce6f8432b064e5837616
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-04-15 13:03:56 +0900

    인텔리제이 기본 자료 추가

---
+++ src/main/java/com/sparta/hh99_clonecoding/Hh99CloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99_clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99CloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99CloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/hh99_clonecoding/Hh99CloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99_clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh99CloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
