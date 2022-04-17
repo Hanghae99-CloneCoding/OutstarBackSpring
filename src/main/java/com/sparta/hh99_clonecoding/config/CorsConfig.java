130d203de1dbf8a8344c14a3da17352e4cfe17d9
Hyunggi <phg9587@naver.com>
2022-04-17 21:58:05 +0900

    fix: cors 설정 관련 코드 수정

--- src/main/java/com/sparta/hh99_clonecoding/config/CorsConfig.java
+++ src/main/java/com/sparta/hh99_clonecoding/config/CorsConfig.java
@@ -1,25 +1,16 @@
 package com.sparta.hh99_clonecoding.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("*", config);
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
     }
-
 }
